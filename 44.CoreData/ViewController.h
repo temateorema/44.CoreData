4f4024b770b2e4ecfe51b835199ef39a321a0bf6
temateorema <md318@MacBook-Pro-md318.local>
2015-06-24 20:43:48 +0300

    Initial Commit

---
+++ 44.CoreData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  44.CoreData
+//
+//  Created by MC723 on 24.06.15.
+//  Copyright (c) 2015 temateorema. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 44.CoreData/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  44.CoreData
+//
+//  Created by MC723 on 24.06.15.
+//  Copyright (c) 2015 temateorema. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
